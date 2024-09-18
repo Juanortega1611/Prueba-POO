6e5b2da978d50bf602ae32280a9abd6eab92e71d
Jose Ortega <josesantanas111110@gmail.com>
2024-09-18 08:07:21 -0700

    metodo suma

--- Form1.cs
+++ Form1.cs
@@ -6,4 +6,18 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+
+    private void Form1_Load(obejct sender, EventArgs e)
+    {
+        var num1 = 7;
+        var num2 = 5;
+        int resultado = Suma(num1, num2);
+
+    }
+
+    private int Suma(int a, int b)
+    {
+        int totalsuma = a + b;
+        return totalsuma;
+    }
 }
