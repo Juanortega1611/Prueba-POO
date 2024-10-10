03217e1918346e803ee8eab21f9ea1a3dc2b20f2
Jose Ortega <josesantanas111110@gmail.com>
2024-10-10 08:49:22 -0700

    Actualizr repositorio

--- FundamentosPOO/Program.cs
+++ FundamentosPOO/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FundamentosPOO.abstractas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,51 @@ namespace FundamentosPOO
 {
     internal class Program
     {
+        //TAREA:
+        //INVESTIGAR LAS CLASES ABSTRACTAS CON EJEMPLOS
         static void Main(string[] args)
         {
-            Persona2 persona2 = 
-                new Persona2("Juan",
-                "josesantanas1110@gmail.com",1);
-            Console.ReadLine();
-            persona2.Nombre = "Angel";
-            persona2.Email = "angel@gmail";
-            persona2.ImprimeNombre();
-            persona2.ImprimeEmail();
-            persona2.CambiarFecha(DateTime.Now);
-            persona2.ImprimeFecha();
+            //Persona2 persona2 = 
+            //    new Persona2("Juan",
+            //"josesantanas1110@gmail.com",1);
+            //Console.ReadLine();
+            // persona2.Nombre = "Angel";
+            // persona2.Email = "angel@gmail";
+            // persona2.ImprimeNombre();
+            // persona2.ImprimeEmail();
+            // persona2.CambiarFecha(DateTime.Now);
+            // persona2.ImprimeFecha();
+
+
+            Cuadrado cuadrado = new Cuadrado()
+            {
+                MedidaLado = 5,
+                UnidadMedida = "Metros"
+            };
+            cuadrado.MuestraArea();
+            cuadrado.MuestraPerimetro();
+            cuadrado.MedidaLado = 8;
+            cuadrado.MuestraArea();
+
+            //TAREA:
+            //HACER LA IMPLEMENTACION PARA RECTANGULO, CIRCULO, TRIANGULO Y POLIGONO
+
             Console.ReadLine();
         }
-    }
+        Rectangulo rectangulo = new Rectangulo()
+        {
+            MedidaLado = 5,
+            UnidadMedida = "Metros"
+        };
+            cuadrado.MuestraArea();
+            cuadrado.MuestraPerimetro();
+            cuadrado.MedidaLado = 8;
+            cuadrado.MuestraArea();
+
+            //TAREA:
+            //HACER LA IMPLEMENTACION PARA RECTANGULO, CIRCULO, TRIANGULO Y POLIGONO
+
+            Console.ReadLine();
+        }
+}
 }
---
+++ FundamentosPOO/abstractas/Rectangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosPOO.abstractas
+{
+    internal class Rectangulo : Figura
+    {
+        public double B;
+        public double H;
+        public int MedidaLado { get; set; }
+        public Rectangulo(double largo, double ancho)
+        {
+            B = largo;
+            H = ancho;
+        }
+
+        public Rectangulo()
+        {
+        }
+
+        public override void CalculaArea()
+        {
+            Double Area = B * H;
+
+        }
+
+        public override void CalcularPerimetro()
+        {
+            Perimetro = MedidaLado * 4;
+        }
+    }
+}
